b50065247dfc203253db9205a811cc2e8f52c1a0
Juhun <etasbyjh@gmail.com>
2018-10-10 20:45:44 -0400

    JH assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 1: make a function that creates a list of vectors has 4 functions: set,get,setInverse,getInverse
+## 2: make a function that either cacluates matrix inversion or returns its cached inversed matrix
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
-    #commit/push test
+      i <- NULL #init null vector for inversed matrix
+      set <- function(y){
+            x <<- y #update x with the new input y
+            i <<- NULL #reset i
+      } 
+      get <- function() x #get
+      setInverse <- function(inverse) i <<- inverse #update i with the newly calculated one
+      getInverse <- function() i #get i
+      list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mcm, ...) {
+      i <- mcm$getInverse() #try to get a cache data from the input, mcm
+      if(!is.null(i)){
+            #if there is, return the cache data
+            message("getting cache data")
+            return(i)
+      }
+      else{
+            #if there isn't, calculate and set the calculated one to the input, mcm
+            im = solve(mcm$get())
+            mcm$setInverse(im)
+      }
+      #then return
+      mcm$getInverse()
 }
